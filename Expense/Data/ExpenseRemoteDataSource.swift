fc5edfd23431fe81d73ba5ca064a7e4b8448ec87
Thiti Sununta <thiti.sununta@clbs.co.th>
2022-10-26 17:09:48 +0700

    add/update func

--- Expense/Data/ExpenseRemoteDataSource.swift
+++ Expense/Data/ExpenseRemoteDataSource.swift
@@ -10,10 +10,10 @@ import Firebase
 import FirebaseFirestore
 
 class ExpenseRemoteDataSource: NSObject {
-
+    
     private let db = Firestore.firestore()
     private let collectionName = "expenses"
-
+    
     func getExpenses(completion: @escaping(_ expenses: [DocumentSnapshot], _ error: String?) -> Void) {
         
         db.collection(collectionName).getDocuments() { (querySnapshot , err) in
@@ -27,8 +27,58 @@ class ExpenseRemoteDataSource: NSObject {
         }
     }
     
-    func saveExpense(expense: Expense) {
+    func saveExpense(expense: Expense, completion: @escaping(_ expense: DocumentSnapshot?, _ error: String?) -> Void) {
         
+        if (expense.identifier != nil){
+            // update
+            let data : [String : Any] = ["title": expense.title ?? "",
+                                         "amount": expense.amount ,
+                                         "catagory": expense.catagory ?? "",
+                                         "created":expense.created ?? Date()]
+            
+            var ref: DocumentReference? = nil
+            ref = db.collection(collectionName).document(expense.identifier!)
+            ref?.updateData(data) { err in
+                if let err = err {
+                    print("Error updating document: \(err)")
+                } else {
+                    print("Document successfully updated")
+                    ref?.getDocument { (document, error) in
+                        if let document = document, document.exists {
+                            completion(document, nil)
+                        } else {
+                            print("Document does not exist")
+                            completion(nil, error?.localizedDescription)
+                        }
+                    }
+                }
+            }
+            
+        }else{
+            // insert
+            let data : [String : Any] = ["title": expense.title ?? "",
+                                         "amount": expense.amount ,
+                                         "catagory": expense.catagory ?? "",
+                                         "created":expense.created ?? Date()]
+            
+            var ref: DocumentReference? = nil
+            ref = db.collection("expenses").addDocument(data: data) { err in
+                if let err = err {
+                    print("Error adding document: \(err)")
+                } else {
+                    print("Document added with ID: \(ref!.documentID)")
+                    
+                    ref?.getDocument { (document, error) in
+                        if let document = document, document.exists {
+                            completion(document, nil)
+                        } else {
+                            print("Document does not exist")
+                            completion(nil, error?.localizedDescription)
+                        }
+                    }
+                }
+            }
+        }
     }
     
     func deleteExpense(expense: Expense) {
