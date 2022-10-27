8d380800f504542cce17c1ddbbbd1dc5d5afe638
Thiti Sununta <thiti.sununta@clbs.co.th>
2022-10-27 17:14:57 +0700

    Impl data repository

--- Expense/Data/ExpenseRepository.swift
+++ Expense/Data/ExpenseRepository.swift
@@ -19,9 +19,35 @@ class ExpenseRepository {
         self.localDataSource = localDataSource
     }
     
+    var cacheIsDirty = false
+    var cachedExpenses:  [String: Expense] = [:]
+
+    
     func getExpenses(completion: @escaping(_ expenses: [Expense], _ error: String?) -> Void) {
 
+        if (!cachedExpenses.isEmpty && !cacheIsDirty) {
+            completion(Array(cachedExpenses.values), nil)
+            return
+        }
+        
+        if (cacheIsDirty) {
+            
+            
+        }else{
+            
+            
+        }
+        
     }
+    
+    private func getExpesesFromRemoteDataSource(completion: @escaping(_ expenses: [Expense], _ error: String?) -> Void) {
+        remoteDataSource.getExpenses { [unowned self] expenses, error in
+            self.refreshCache(expenses: expenses)
+            self.refreshLocalDataSource(expenses: expenses)
+            completion(Array(self.cachedExpenses.values), nil)
+        }
+    }
+    
     
     func saveExpense(expense: Expense) {
         
@@ -31,4 +57,35 @@ class ExpenseRepository {
         
     }
     
+    private func refreshCache(expenses: [Expense]) {
+        cachedExpenses.removeAll()
+        expenses.forEach { expense in
+            _ = cacheAndPerform(expense: expense)
+        }
+        cacheIsDirty = false
+    }
+    
+    private func refreshLocalDataSource(expenses: [Expense]) {
+        localDataSource.deleteAll()
+        for expense in expenses {
+            localDataSource.saveExpense(expense: expense)
+        }
+
+    }
+
+    private func cacheAndPerform(expense: Expense) -> Expense {
+        let cached = Expense(identifier: expense.identifier,
+                             title: expense.title,
+                             detail: expense.detail,
+                             amount: expense.amount,
+                             type: expense.type,
+                             catagory: expense.catagory,
+                             created: expense.created)
+        
+        if let identifier = cached.identifier {
+            cachedExpenses[identifier] = cached
+        }
+        return cached
+    }
+
 }
